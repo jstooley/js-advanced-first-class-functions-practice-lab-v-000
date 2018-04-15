a761b93efafdcfc22f6cccd29bcced53778686b0
Justin Tooley <jstooley823@gmail.com>
2018-04-15 01:04:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solution in this file!
+const logDriverNames = function (driver) {
+  for (i = 0 ; i < driver.length ; i++){
+    console.log(driver[i].name)
+  }
+}
+function logDriversByHometown(drivers, location){
+  let matches = drivers.filter(driver => Object.is(driver.hometown, location))
+  logDriverNames(matches)
+}
+const driversByRevenue = (driver) => {
+  return driver.slice().sort(function (a,b) {
+    return a.revenue - b.revenue
+  })
+}
+const driversByName = (drivers) => {
+  return drivers.slice().sort(function(a,b){
+    return a.name.localeCompare(b.name)
+  })
+}
+function totalRevenue(driver){
+  for (i = 0; i < driver.length ; i++ ){
+    
+  }
+}
